2960be7ec9e1cd949f41027b49d8c793cac90804
wilmer <electronik_pc@yahoo.es>
2022-04-27 20:05:13 -0400

    inicio theme

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Jesús Martínez | Desarrollador Backend</title>
+</head>
+<body>
+
+
+  <header class="head">
+      <div class="head__logo"><img src="asset/img/avatar.png" alt="" srcset=""></div>
+      <div class="head__menu"><a href="#">HOME</a></div>
+  </header>
+    
+</body>
+</html>
